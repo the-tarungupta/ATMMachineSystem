1f7fe20a60e08418432f6d166a7ebe1b03eedcc5
Tarun Gupta <168983071+the-tarungupta@users.noreply.github.com>
2024-10-13 19:17:58 +0530

    Formatted the Code

--- ATMSystem.java
+++ ATMSystem.java
@@ -1,153 +1,168 @@
-import java.util.Scanner;
+import java.util.*;
 
-class BankAccount {
-    private double balance;
+class BankAccount{
+    private int balance;
     private int PIN;
 
-    BankAccount(int initialBalance , int inputPin){
+    BankAccount(int initialBalance , int PIN){
         balance = initialBalance;
-        PIN = inputPin;
+        this.PIN = PIN;
     }
 
-    public double checkBalance(){
+    public int checkBalance(){
         return balance;
     }
 
+    public boolean checkPin(int PIN){
+        return this.PIN == PIN;
+    }
 
-    public void depositMoney(double amount){
-        if(amount > 0){
-            balance += amount;
-            System.out.println(amount+" amount deposited successfully");
+    public void withdrawMoney(int amount){
+        if(amount<=balance){
+            balance-=amount;
+            System.out.println(amount+" withdrawan successfully");
+            System.out.println();
         }
         else{
-            System.out.println("Invalid deposit amount");
+            System.out.println("Insufficient balance in your account");
+            System.out.println();
         }
     }
 
 
-    public void withdrawMoney(double amount){
-        if(amount>0 && amount<=balance){
-            balance -= amount;
-            System.out.println(amount+" amount withdrawal successfully");
+    public void depositMoney(int amount){
+        if(amount>0){
+            balance += amount;
+            System.out.println(amount+" deposited successfully");
+            System.out.println();
         }
-        else if(amount<=0){
-            System.out.println("Invalid amount for withdrawals");
-        }
-        else if(amount>balance){
-            System.out.println("Insufficient balance");
+        else{
+            System.out.println("Invalid amount entered");
+            System.out.println();
         }
     }
 
 
-    public boolean verifyPin(int inputPIN){
-        return PIN == inputPIN;
+    public void changePin(int PIN){
+        this.PIN = PIN;
+        System.out.println("Your PIN changed successfully");
+        System.out.println();
     }
-
-
-    public void changePin(int inputPIN){
-        PIN = inputPIN;
-        System.out.println("PIN changed successfully");
-    }
-
-
 }
 
-public class ATMSystem {
+class ATMSystem {
     public static void main(String[] args) {
 
+        BankAccount account = new BankAccount(2000 , 9956);
         Scanner sc = new Scanner(System.in);
 
-        BankAccount account = new BankAccount(1000 , 1234);
+        System.out.println("--------------------------ATM Machine System---------------------------");
+        System.out.println("Here are your Main Menu options : ");
+        System.out.println();
         boolean exit = true;
-
         while(exit){
-            System.out.println("\nATM Menu:");
-            System.out.println("1. Check Balance");
-            System.out.println("2. Withdraw Money");
-            System.out.println("3. Deposit Money");
-            System.out.println("4. Change PIN");
-            System.out.println("5. Exit");
-            System.out.print("Choose an option: ");
+            System.out.println("1]. Check Balance");
+            System.out.println("2]. Withdraw Money");
+            System.out.println("3]. Deposit Money");
+            System.out.println("4]. Change PIN");
+            System.out.println("5]. Exit");
+            System.out.println();
 
+            System.out.print("Enter your choice : ");
             try{
-                    int choice = sc.nextInt();
+                int choice = sc.nextInt();
+                switch(choice){
+                    case 1 : int pin = validPin(sc);
 
-                    if (choice < 4 && choice >= 1) {
-                        System.out.print("Enter your PIN : ");
-                        try {
-                            int inputPIN = sc.nextInt();
-                            if( !account.verifyPin(inputPIN)){
-                                System.out.println("Invalid PIN. Try again");
-                                continue;
-                            }
+                        if(account.checkPin(pin)){
+                            int balance = account.checkBalance();
+                            System.out.println("Available Balance : "+balance);
+                            System.out.println();
                         }
-                        catch (Exception e){
-                            System.out.println("Error : Enter 4-digit PIN");
-                            sc.next();
-                            continue;
+                        else{
+                            System.out.println("Incorrect PIN. Try again");
+                            System.out.println();
                         }
-                    }
-
-                    switch (choice) {
-                        case 1:
-                            System.out.println("Current balance : " + account.checkBalance());
-                            break;
-                        case 2:
-                            System.out.print("Enter amount to withdraw: ");
-                            double withdrawAmount = sc.nextDouble();
-                            account.withdrawMoney(withdrawAmount);
-                            break;
-                        case 3:
-                            System.out.println("Enter amount to deposit: ");
-                            double depositAmount = sc.nextDouble();
-                            account.depositMoney(depositAmount);
-                            break;
-                        case 4:
-
-                            try {
-
-                                int newPIN;
-
-                                    System.out.print("Enter your old PIN : ");
-                                    int inputPIN = sc.nextInt();
-                                    if (!account.verifyPin(inputPIN)) {
-                                        System.out.println("Invalid PIN. Try again");
-                                        continue;
-                                    }
-                                do{
-                                    System.out.print("Enter your new PIN: ");
-                                    newPIN = sc.nextInt();
-
-                                    if (inputPIN == newPIN) {
-                                        System.out.println("New pin cannot be same as old pin. Please enter different pin. ");
-
-                                    }
-                                }while (inputPIN == newPIN);
-                                account.changePin(newPIN);
+                        continue;
+                    case 2 : pin = validPin(sc);
+                        if(account.checkPin(pin)){
+                            System.out.print("Enter amount to be withdrawan : ");
+                            int amount = sc.nextInt();
+                            account.withdrawMoney(amount);
+                        }
+                        else{
+                            System.out.println("Incorrect PIN. Try again");
+                            System.out.println();
+                        }
+                        continue;
+                    case 3 : pin = validPin(sc);
+                        if(account.checkPin(pin)){
+                            System.out.print("Enter amount to be depsoited : ");
+                            int amount = sc.nextInt();
+                            account.depositMoney(amount);
+                        }
+                        else{
+                            System.out.println("Incorrect PIN. Try again");
+                            System.out.println();
+                        }
+                        continue;
+                    case 4 : pin = validPin(sc);
+                        if(account.checkPin(pin)){
+                            try{
+                                System.out.print("Enter your new PIN : ");
+                                int newPIN = sc.nextInt();
+                                if(String.valueOf(newPIN).length() !=4){
+                                    System.out.println("Your new PIN must be 4-digit. Try again.");
+                                }
+                                else{
+                                    account.changePin(newPIN);
+                                }
                             }
-                            catch (Exception e){
-                                System.out.println("Error : Enter 4-digit PIN");
+                            catch(Exception e){
+                                System.out.println("Error! Your PIN must be in digits.");
                                 sc.next();
-                                continue;
                             }
+                        }
+                        else{
+                            System.out.println("Incorrect PIN. Try again");
+                            System.out.println();
+                        }
+                        continue;
+                    case 5 : System.out.println("Exiting....Thank you for using our ATM Service ");
+                        exit = false;
+                        break;
+                    default : System.out.println("Invalid Choice. Choose 1 to 5 digit ! ");
+                        continue;
 
-                            break;
-                        case 5:
-                            exit = false;
-                            System.out.println("Exiting .... Thankyou for using the ATM.");
-                            break;
-                        default:
-                            System.out.println("Invalid choice. Please try again.");
-                    }
+                }
             }
-
             catch(Exception e){
-                System.out.println("Error : You have to enter a value b/w 1 to 4 ");
-                sc.next();     // showing main menu options after throwing error
+                System.out.println("Error! You have to enter a digit");
+                sc.next();
             }
         }
 
+    }
 
-
+    public static int validPin(Scanner sc){
+        int PIN;
+        while(true){
+            try{
+                System.out.print("Enter your PIN : ");
+                PIN = sc.nextInt();
+                if(String.valueOf(PIN).length() != 4){
+                    System.out.println("Your PIN must be 4-digit. Try again.");
+                    continue;
+                }
+                else{
+                    break;
+                }
+            }
+            catch(Exception e){
+                System.out.println("Error! Your PIN must be in digits.");
+                sc.next();
+            }
+        }
+        return PIN;
     }
 }
